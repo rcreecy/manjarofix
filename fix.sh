84e1f8e37715b82ae2c659c3cbe0f008a3ab2ed9
rcreecy <rcreecy@icloud.com>
2020-03-22 15:06:57 -0500

    fixed syntax

--- fix.sh
+++ fix.sh
@@ -2,6 +2,6 @@ rm -f /etc/pacman.d/mirrorlist
 echo "Original Mirrorlist deleted properly"
 cp ./mirrorlist.txt /etc/pacman.d/mirrorlist
 ls -la /etc/pacman.d
-cat /etc/pacman.d/mirrorlist
-wc -l /etc/pacman.d/mirrorlist
+pacman -Syu
 pacman -Sy gnome-tweaks
+wc -l /etc/pacman.d/mirrorlist
