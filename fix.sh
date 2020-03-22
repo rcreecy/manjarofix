f32080d0fcb2af720b1d3976c54b31a096f7ae05
rcreecy <rcreecy@icloud.com>
2020-03-22 14:46:02 -0500

    fixed syntax

--- fix.sh
+++ fix.sh
@@ -3,5 +3,5 @@ echo "Original Mirrorlist deleted properly"
 cp ./mirrorlist.txt /etc/pacman.d/mirrorlist
 ls -la /etc/pacman.d
 cat /etc/pacman.d/mirrorlist
-$line_count = 'wc -l /etc/pacman.d/mirrorlist'
+$line_count = '$(wc -l /etc/pacman.d/mirrorlist)'
 echo "Line count in mirror list = $line_count"
